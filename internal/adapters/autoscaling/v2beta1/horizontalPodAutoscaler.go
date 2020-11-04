a99c7026ed729d0e66b9ec7f1ddf0025787cfb7f
William Artero <williamwmoraes@gmail.com>
2020-11-04 22:05:46 +0100

    feat: link HPA to deployments

--- internal/adapters/autoscaling/v2beta1/horizontalPodAutoscaler.go
+++ internal/adapters/autoscaling/v2beta1/horizontalPodAutoscaler.go
@@ -2,11 +2,12 @@ package v2beta1
 
 import (
 	"fmt"
-	"log"
+	"os"
 	"reflect"
 
 	"github.com/goccy/go-graphviz/cgraph"
 	"github.com/wwmoraes/kubegraph/internal/adapter"
+	appsV1 "k8s.io/api/apps/v1"
 	autoscalingV2beta1 "k8s.io/api/autoscaling/v2beta1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
@@ -54,6 +55,11 @@ func (thisAdapter horizontalPodAutoscalerAdapter) Connect(statefulGraph adapter.
 
 // Configure connects the resources on this adapter with its dependencies
 func (thisAdapter horizontalPodAutoscalerAdapter) Configure(statefulGraph adapter.StatefulGraph) error {
+	deploymentAdapter, err := adapter.Get(reflect.TypeOf(&appsV1.Deployment{}))
+	if err != nil {
+		return fmt.Errorf("warning[%s configure]: %v", thisAdapter.GetType().String(), err)
+	}
+
 	objects, err := statefulGraph.GetObjects(thisAdapter.GetType())
 	if err != nil {
 		return err
@@ -68,8 +74,11 @@ func (thisAdapter horizontalPodAutoscalerAdapter) Configure(statefulGraph adapte
 			return err
 		}
 
-		// do something with each resource
-		log.Printf("%s resource %s, node %s", thisAdapter.GetType().String(), resource.Name, resourceNode.Name())
+		if resource.Spec.ScaleTargetRef.Kind == "Deployment" && resource.Spec.ScaleTargetRef.APIVersion == "apps/v1" {
+			deploymentAdapter.Connect(statefulGraph, resourceNode, resource.Spec.ScaleTargetRef.Name)
+		} else {
+			fmt.Fprintf(os.Stderr, "unknown scaleRef %s.%s\n", resource.Spec.ScaleTargetRef.APIVersion, resource.Spec.ScaleTargetRef.Kind)
+		}
 	}
 	return nil
 }
