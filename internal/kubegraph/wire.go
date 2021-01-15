00d28c3211c63209908d7ae42584c500dba4e362
William Artero <williamwmoraes@gmail.com>
2021-01-15 08:34:32 +0100

    refactor: move decode function and schema init to adapters + inject on kubegraph

--- internal/kubegraph/wire.go
+++ internal/kubegraph/wire.go
@@ -6,8 +6,9 @@ import (
 	"github.com/google/wire"
 	"github.com/wwmoraes/dot"
 	"github.com/wwmoraes/kubegraph/internal/adapter"
+	"github.com/wwmoraes/kubegraph/internal/adapters"
 )
 
 func InitializeKubegraph(optionsFn ...dot.GraphOptionFn) (*KubeGraph, error) {
-	panic(wire.Build(NewKubegraph, dot.New, adapter.RegistryInstance))
+	panic(wire.Build(NewKubegraph, dot.New, adapter.RegistryInstance, adapters.GetDecoder))
 }
