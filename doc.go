2c411f63435c1e23b155a31313a2f55d2f3f6558
William Artero <williamwmoraes@gmail.com>
2021-01-19 15:33:23 +0100

    docs: main package description

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package kubegraph provides a CLI tool that parses kubernetes resources from
+// local YAML or JSON data into a directed graph using the dot language, i.e.
+// graphviz-compatible graphs.
+//
+// Copyright (c) William Artero. MIT License
+package kubegraph // import "github.com/wwmoraes/kubegraph"
